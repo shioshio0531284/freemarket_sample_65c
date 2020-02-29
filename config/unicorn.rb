1ff1ae58a7f1d9baf92022639b5843fb98413a8b
Yawara-1014 <urumajin@gmail.comm>
2020-02-29 21:08:33 +0900

    config unicorn listen change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -34,7 +34,7 @@ run_once = true
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
-    
+
     if run_once
       run_once = false # prevent from firing again
     end
