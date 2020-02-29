151dcae9eef5db8c2eaa64038c983fe128717197
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-02-29 15:58:09 +0900

    Signup_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firsst_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :birth_year, :birth_month, :birth_dey, :phone_number])
   end
 end
