31b42e44b49fc16feb99b505c6a9efe3de0fe235
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-01 19:21:39 +0900

    wizard_ implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :birth_date, :phone_number, presence: true
+  has_one :users_address
 end
