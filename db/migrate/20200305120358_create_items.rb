a1b26998e1e37505b4c08cfb93c2997eaab963bb
Yawara-1014 <urumajin@gmail.comm>
2020-03-07 15:07:46 +0900

    item migrations add

--- db/migrate/20200305120358_create_items.rb
+++ db/migrate/20200305120358_create_items.rb
@@ -1,8 +1,20 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name ,  null:false
+      t.string :image,  null:false
+      t.description :text, null:false
+      t.status :text, null:false
+      t.postage :string, null:false
+      t.delivery_way :string, null:false
+      t.delivery_area :string, null:false
+      t.delivery_date :string, null:false
+      t.price :integer, null:false
+      t.category :text
+      t.size :text
+      t.brand :text
       t.timestamps
     end
   end
 end
+
