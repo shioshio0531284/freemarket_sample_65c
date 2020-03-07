017d07a5a6cbf7094c33ee2013acc0196e4617bc
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-07 21:52:31 +0900

    user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :birth_date, :phone_number, presence: true
+  validates :first_name, :last_name, :nickname, :birth_date, presence: true
+
+  validates :first_name_kana, :last_name_kana,
+               format: { with: /\A[ぁ-んー－]+\z/}, presence: true
+
+  validates :phone_number,
+               format: { with: /\A\d{10,11}\z/}, presence: true
+
   has_one :users_address
 end
