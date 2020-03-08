8ec0578d7b6d75cb4ddd0e4f0827db629a24fed6
Yawara-1014 <urumajin@gmail.comm>
2020-03-08 11:58:05 +0900

    item controller create add

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,15 @@ class ItemsController < ApplicationController
   end
 
   def create
+    item = Item.new(item_params)
+    item.save!
+    redirect_to root_path, notice: "新規商品「#{item.name}」を登録しました。"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
   end
 
 end
