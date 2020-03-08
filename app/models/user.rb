a923a370f3f3939c85ae7d1a30cad5c611e746f0
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-08 18:39:49 +0900

    phone_number_validation_delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :nickname, :birth_date, :first_name_kana, :last_name_kana, :phone_number, presence: true
+  validates :first_name, :last_name, :nickname, :birth_date, :first_name_kana, :last_name_kana, presence: true
 
   validates :first_name_kana, :last_name_kana, format: { with: /\A[ぁ-んー－]+\z/, message: "は全角ひらがなで入力してください" }
-
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "はハイフン無しの10桁か11桁の番号を入力してください" }
 
   has_one :users_address
 end
