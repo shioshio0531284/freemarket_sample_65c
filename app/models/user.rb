33ccbbf5fc8e119e0bb9e5452b3c6a50677f0172
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-08 17:33:32 +0900

    model_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :nickname, :birth_date, presence: true
+  validates :first_name, :last_name, :nickname, :birth_date, :first_name_kana, :last_name_kana, :phone_number, presence: true
 
   validates :first_name_kana, :last_name_kana,
-               format: { with: /\A[ぁ-んー－]+\z/}, presence: true
+               format: { with: /\A[ぁ-んー－]+\z/,message: "は全角ひらがなで入力してください"}
 
   validates :phone_number,
-               format: { with: /\A\d{10,11}\z/}, presence: true
+               format: { with: /\A\d{10,11}\z/,message: "はハイフン無しの10桁か11桁の番号を入力してください"}
+
 
   has_one :users_address
 end
