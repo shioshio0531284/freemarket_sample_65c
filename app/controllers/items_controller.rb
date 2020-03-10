0373940ff7fb3e7cf4249450836d65b754803de7
Yawara-1014 <urumajin@gmail.comm>
2020-03-10 19:45:41 +0900

    [fix]items_controller #index.new.create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def new
     @item = Item.new
+    @item.images.new
   end
 
   def create
-    item = Item.new(item_params)
-    item.save!
-    redirect_to root_path, notice: "新規商品「#{item.name}」を登録しました。"
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description)
+    params.require(:item).permit(:name, :price, images_attributes: [:src])
   end
 
 end
