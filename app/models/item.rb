38cae6b9bdfc49cbe0eca2b9fcbf079ec59e1ddf
Yawara-1014 <urumajin@gmail.comm>
2020-03-11 19:04:45 +0900

    [add]item #user,optional:user

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :name, :price, presence: true
