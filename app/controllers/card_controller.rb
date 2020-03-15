1ce7cec8b568e85eac812e174c28f08f31d2217f
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-15 15:22:01 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -4,7 +4,7 @@ class CardsController < ApplicationController
 
   def index
     if @card.present?
-      Payjp.api_key = "秘密鍵"
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @card_information = customer.cards.retrieve(@card.card_id)
       @card_brand = @card_information.brand      
