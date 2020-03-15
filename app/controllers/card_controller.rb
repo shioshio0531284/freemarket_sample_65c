5440f93ae591677b2d3d22eed902c44cf4a311f0
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-15 15:14:16 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,15 +2,36 @@ class CardsController < ApplicationController
   require "payjp"
   before_action :set_card
 
+  def index
+    if @card.present?
+      Payjp.api_key = "秘密鍵"
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_information = customer.cards.retrieve(@card.card_id)
+      @card_brand = @card_information.brand      
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
+    end
+  end
+
   def new
     card = Card.where(user_id: current_user.id).first
     redirect_to action: "index" if card.present?
   end
 
- # indexアクションはここでは省略
-
   def create
-    Payjp.api_key = '秘密鍵'
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
 
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -30,9 +51,21 @@ class CardsController < ApplicationController
     end
   end
 
+  def destroy
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    customer.delete
+    if @card.destroy
+      redirect_to action: "index", notice: "削除しました"
+    else
+      redirect_to action: "index", alert: "削除できませんでした"
+    end
+  end
+
   private
 
   def set_card
     @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
+
 end
