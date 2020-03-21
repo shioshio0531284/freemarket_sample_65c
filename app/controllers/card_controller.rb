e8f49e91065f022343e9df0ce5a50df901070149
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-03-21 15:05:31 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -12,11 +12,11 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      description: '登録テスト', #なくてもOK
-      email: current_user.email, #なくてもOK
+      description: '登録テスト'
+      email: current_user.email
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+      )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
@@ -26,7 +26,7 @@ class CardController < ApplicationController
     end
   end
 
-  def delete #PayjpとCardデータベースを削除します
+  def delete
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
@@ -38,7 +38,7 @@ class CardController < ApplicationController
       redirect_to action: "new"
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出します
+  def show
     card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to action: "show" 
