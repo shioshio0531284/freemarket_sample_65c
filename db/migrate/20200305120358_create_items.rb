b50a0e03f2391e08d7e53280cc5b99e5a2fed449
Yawara-1014 <urumajin@gmail.comm>
2020-04-04 15:58:30 +0900

    [add]items_validates

--- db/migrate/20200305120358_create_items.rb
+++ db/migrate/20200305120358_create_items.rb
@@ -2,15 +2,15 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name ,  null:false
-      t.text :description
-      t.integer :status, default: 0
-      t.integer :postage, default: 0
-      t.integer :delivery_way, default: 0
-      t.integer :delivery_area,default: 0
-      t.integer :delivery_day,default: 0
+      t.text :description, null:false
+      t.integer :status, null:false
+      t.integer :postage, null:false
+      t.integer :delivery_way, null:false
+      t.integer :delivery_area, null:false
+      t.integer :delivery_day, null:false
       t.integer :price, null:false
-      t.integer :category, default: 0
-      t.integer :size, default: 0 
+      t.integer :category, null:false
+      t.integer :size
       t.text :brand
       t.references :user, null: false, foreign_key: true
       t.timestamps
