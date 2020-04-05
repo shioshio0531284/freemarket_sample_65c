50339e80e26c417814121e068f129413fa9b113a
Yawara-1014 <urumajin@gmail.comm>
2020-04-05 19:54:37 +0900

    [add]商品出品flash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: "商品を出品しました"
     else
       redirect_to new_item_path, flash: { error: @item.errors.full_messages }
     end
