a53783846faab13a55c6d539dd7266872746d6f0
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-04-05 15:12:55 +0900

    Update purchase_controller.rb

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,17 +1,14 @@
 class PurchaseController < ApplicationController
   require 'payjp'
+  before_action :set_item
 
   def index
     card = Card.where(user_id: current_user.id).first
-    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
-      #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
       Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
-      #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
-      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
   end
@@ -20,10 +17,17 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
-  )
-  redirect_to action: 'done' #完了画面に移動
+    :amount => @item.price,
+    :customer => card.customer_id,
+    :currency => 'jpy'
+    )
+    
+    redirect_to action: 'done'
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
